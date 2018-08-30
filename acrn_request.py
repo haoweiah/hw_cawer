8fe103f7c95127474240344323dacf53e90cf433
Wei Liu <weix.w.liu@intel.com>
2018-08-30 11:15:57 +0800

    发邮件基本功能实现，rebase功能因为用户认证原因，之后可以实现

--- acrn_request.py
+++ acrn_request.py
@@ -39,15 +39,15 @@ class ProjectacrnPullRequest(object):
         msg['Subject'] = subject
         msg['From'] = sender
         msg['TO'] = ','.join(receivers)
-        print(subject)
-        print(content)
-        print(receivers)
-        # try:
-        #     s = smtplib.SMTP('smtp.intel.com')
-        #     s.sendmail(sender, receivers, msg.as_string())
-        #     logging.info('send suess')
-        # except smtplib.SMTPException as err:
-        #     logging.info('Failed to send mail\n{}'.format(err))
+        # print(subject)
+        # print(content)
+        # print(receivers)
+        try:
+            s = smtplib.SMTP('smtp.intel.com')
+            s.sendmail(sender, receivers, msg.as_string())
+            logging.info('send suess')
+        except smtplib.SMTPException as err:
+            logging.info('Failed to send mail\n{}'.format(err))
 
     def acrn_url_info(self, url):
         # request data
@@ -80,18 +80,22 @@ class ProjectacrnPullRequest(object):
             f.write(str(num_list))
 
     def checkon(self, num, commit_url):
-        message = self.acrn_url_info(commit_url)['message']
-        list = re.findall(r'Tracked-On: #(\d+)', message)
-        mail = re.findall(r'Signed-off-by:.*?<(.*?@intel.com)>', message)
+        try:
+            message = self.acrn_url_info(commit_url)[0]['commit']['message']
+            list = re.findall(r'Tracked-On: #(\d+)', message)
+            mail = re.findall(r'Signed-off-by:.*?<(.*?@intel.com)>', message)
+        except Exception as e:
+            logging.info('commit链接获取错误', e)
+            return False
         if not list:
             if not mail:
-                print('未找到mail 邮件发给谁')
+                logging.info('未找到mail 邮件发给谁')
                 subject = 'PR %d 未找到mail联系人' % num
                 content = 'PR %d 未找到mail联系人， Tracked-On信息未填写' % num
                 mail.append('minxia.wang@intel.com')
                 self.send_email(subject, content)
             else:
-                print('找到mail发邮件', mail)
+                logging.info('找到mail发邮件', mail)
                 subject = 'PR %d 未填写Tracked-On信息' % num
                 content = 'PR %d 未填写Tracken-On信息' % num
                 mail.append('minxia.wang@intel.com')
@@ -108,18 +112,18 @@ class ProjectacrnPullRequest(object):
                     break
                 list = re.findall(r'\[External_System_ID\]', ID)
             if not list:
-                print('未找到ID发邮件 需要发送给5个人', mail)
+                logging.info('未找到ID发邮件 需要发送给5个人', mail)
                 subject = 'issues 问题'
                 content = 'PR {} 的issues {} 没有 External_System_ID '.format(num, issues_num)
                 self.send_email(subject, content)
                 return False
         except Exception as e:
-            print('issuers链接错误发邮件 需要发送给5个人', mail)
+            logging.info('issuers链接错误发邮件 需要发送给5个人', mail)
             subject = 'issues的连接错误，可能是Tracken-On信息错误'
             content = 'issues的连接错误，可能是Tracken-On信息错误'
-            self.send_email(subject, content, mail)
+            self.send_email(subject, content)
             return False
-        print('checkon OK')
+        logging.info('checkon OK')
 
     def projectcarn_merge_rebase(self):
         # Parse the json data and execute the method
@@ -133,40 +137,40 @@ class ProjectacrnPullRequest(object):
             base = pulls_json[i]['base']['ref']
             num = pulls_json[i]['number']
             commits_url = pulls_json[i]['commits_url']
-            print(num)
+            # print(num)
             self.checkon(num, commits_url)
-        #     num_url = pulls_json[i]['url']
-        #     num = pulls_json[i]['number']
-        #     comment_url = pulls_json[i]['comments_url']
-        #     statuses_url = pulls_json[i]['statuses_url']
-        #     review_url = pulls_json[i]['url'] + '/reviews'
-        #     review_json = self.acrn_url_info(review_url)
-        #     for review in review_json:
-        #         user = review["user"]["login"]
-        #         if review.get('state') == "APPROVED" and (user == "anthonyzxu" or user == "dongyaozu"):
-        #             check_json = self.acrn_url_info(statuses_url)
-        #             if check_json[0]['state'] == 'success':
-        #                 merge_num_dict[num] = [0, comment_url]
-        #                 send_num_dict[num] = num_url
-        #
-        # merge_num_list = sorted([key for key in merge_num_dict.keys()])
-        # read_num_list = sorted([key for key in read_num_dict.keys()])
-        # logging.info('merge_num_list %s' % merge_num_list)
-        # logging.info('read_num_list %s' % read_num_list)
-        # if not operator.eq(read_num_list, merge_num_list):
-        #     # 使用operator判断两个列表是否相同
-        #     logging.info("可以rebase编号：%s" % merge_num_list)
-        #     self.send_email(json.dumps(send_num_dict))
-        #     for num, list in merge_num_dict.items():
-        #         if num in read_num_list:
-        #             merge_num_dict[num][0] = 1
-        #             list[0] = 1
-        #         if list[0] == 0:
-        #             status_code = self.post_comments(list[1])
-        #             merge_num_dict[num][0] = 1
-        #             body = "修改成功" if status_code == 201 else "修改失败"
-        #             logging.info(body)
-        #     self.write_file(merge_num_dict)
+            num_url = pulls_json[i]['url']
+            num = pulls_json[i]['number']
+            comment_url = pulls_json[i]['comments_url']
+            statuses_url = pulls_json[i]['statuses_url']
+            review_url = pulls_json[i]['url'] + '/reviews'
+            review_json = self.acrn_url_info(review_url)
+            for review in review_json:
+                user = review["user"]["login"]
+                if review.get('state') == "APPROVED" and (user == "anthonyzxu" or user == "dongyaozu"):
+                    check_json = self.acrn_url_info(statuses_url)
+                    if check_json[0]['state'] == 'success':
+                        merge_num_dict[num] = [0, comment_url]
+                        send_num_dict[num] = num_url
+
+        merge_num_list = sorted([key for key in merge_num_dict.keys()])
+        read_num_list = sorted([key for key in read_num_dict.keys()])
+        logging.info('merge_num_list %s' % merge_num_list)
+        logging.info('read_num_list %s' % read_num_list)
+        if not operator.eq(read_num_list, merge_num_list):
+            # 使用operator判断两个列表是否相同
+            logging.info("可以rebase编号：%s" % merge_num_list)
+            self.send_email(json.dumps(send_num_dict))
+            for num, list in merge_num_dict.items():
+                if num in read_num_list:
+                    merge_num_dict[num][0] = 1
+                    list[0] = 1
+                if list[0] == 0:
+                    status_code = self.post_comments(list[1])
+                    merge_num_dict[num][0] = 1
+                    body = "修改成功" if status_code == 201 else "修改失败"
+                    logging.info(body)
+            self.write_file(merge_num_dict)
 
 
 if __name__ == '__main__':
